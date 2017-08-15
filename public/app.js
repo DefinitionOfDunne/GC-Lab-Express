ea39bd63bbfb34c9b55416ec44d550ac391103a8
Dylan Dunne <dylanadunne@gmail.com>
2017-08-15 14:35:32 -0400

    added images and angular FE

--- public/app.js
+++ public/app.js
@@ -2,7 +2,7 @@ angular.module("quoteApp", [])
 .controller("quoteController", function($scope, $http) {
 
     $http.get("/random").then(function(response) {
-        console.log(response.data);
+        console.log(response.data.author);
         $scope.quote = response.data;
     })
 });
--- server.js
+++ server.js
@@ -6,19 +6,58 @@ app.use(express.static(__dirname + "/public"));
 
 app.get('/random', function (req, res) {
     var collection = [
-        "We build our programs the way we build our cities: over time, without a plan, on top of ruins. - Ellen Ullman",
-        "What one programmer can do in one month, two programmers can do in two months. - Fred Brooks",
-        "Every great developer you know got there by solving problems they were unqualified to solve until they actually did it. - Patrick McKenzie",
-        "Rules of Optimization: Rule 1 - Don't do it. Rule 2 - (for experts only): Don't do it yet. - Michael A. Jackson",
-        "A primary cause of complexity is that software vendors uncritically adopt almost any feature that users want.- Niklaus Wirth",
-        "No one in the brief history of computing has ever written a piece of perfect software. It's unlikely that you'll be the first. - Andy Hunt",
-        "Java is to JavaScript as ham is to hamster. - Jeremy Keith",
-        "One of the best programming skills you can have is knowing when to walk away for awhile. - Oscar Godson",
-        "Without requirements or design, programming is the art of adding bugs to an empty text file. - Louis Srygley",
-        "In the happy land of elegant code and pretty rainbows, there lives a spoil-sport monster called inefficiency - Marijn Haverbeke"
+        {
+            quote:"What one programmer can do in one month, two programmers can do in two months.",
+            author: "Fred Brooks", 
+            image: "images/brooks.jpg"
+        },
+                {
+            quote:"Every great developer you know got there by solving problems they were unqualified to solve until they actually did it.",
+            author: "Patrick McKenzie", 
+            image: "images/mckenzie.jpg"
+        },
+                {
+            quote:"Rules of Optimization: Rule 1 - Don't do it. Rule 2 - (for experts only): Don't do it yet.",
+            author: "Michael A. Jackson", 
+            image: "images/jackson.jpg"
+        },
+                {
+            quote: "A primary cause of complexity is that software vendors uncritically adopt almost any feature that users want.",
+            author: "Niklaus Wirth", 
+            image: "images/wirth.jpg"
+        },
+                {
+            quote:"No one in the brief history of computing has ever written a piece of perfect software. It's unlikely that you'll be the first.",
+            author: "Andy Hunt", 
+            image: "images/ullman.jpg"
+        },
+                {
+            quote:"We build our programs the way we build our cities: over time, without a plan, on top of ruins.",
+            author: "Ellen Ullman", 
+            image: "images/ullman.jpg"
+        },{
+            quote:"Without requirements or design, programming is the art of adding bugs to an empty text file.",
+            author: "Louis Srygley", 
+            image: "images/srygley.jpg"
+        },{
+            quote:"One of the best programming skills you can have is knowing when to walk away for awhile.",
+            author: "Oscar Godson", 
+            image: "images/godson.jpg"
+        },{
+            quote: "Java is to JavaScript as ham is to hamster.",
+            author: "Jeremy Keith", 
+            image: "images/keith.jpg"
+        },
+        {
+            quote: "In the happy land of elegant code and pretty rainbows, there lives a spoil-sport monster called inefficiency.",
+            author: "Marijn Haverbeke", 
+            image: "images/haverbeke.jpg"
+        }
+       
     ];
 
     var randomized = collection[Math.floor(Math.random() * collection.length)];
+    console.log(randomized);
 
   res.send(randomized);
 });
